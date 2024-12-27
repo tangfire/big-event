39405e51d5ede1813686a098c5eb0a63cddf93a7
tangfire <52628216@qq.com>
2024-12-27 17:03:45 +0800

    update password!

--- src/main/java/com/fire/bigevent/config/OssConfiguration.java
+++ src/main/java/com/fire/bigevent/config/OssConfiguration.java
@@ -15,14 +15,14 @@ import org.springframework.context.annotation.Configuration;
 @Slf4j
 public class OssConfiguration {
 
-    // Endpoint以华东1（杭州）为例，其它Region请按实际情况填写。
-//    private static final String ENDPOINT = "oss-cn-guangzhou.aliyuncs.com";
-//    // 从环境变量中获取访问凭证。运行本代码示例之前，请确保已设置环境变量OSS_ACCESS_KEY_ID和OSS_ACCESS_KEY_SECRET。
-////        EnvironmentVariableCredentialsProvider credentialsProvider = CredentialsProviderFactory.newEnvironmentVariableCredentialsProvider();
-//    private static final String ACCESS_KEY_ID = "LTAI5tSBSPcZ5sqftNiVkZts";
-//    private static final String ACCESS_KEY_SECRET = "a9Iwh9oS3crMmRoND4L1HMRZrqz0Z3";
-//    // 填写Bucket名称，例如examplebucket。
-//    private static final String BUCKETNAME = "sky-fire-itcast";
+//     Endpoint以华东1（杭州）为例，其它Region请按实际情况填写。
+    private static final String ENDPOINT = "oss-cn-guangzhou.aliyuncs.com";
+    // 从环境变量中获取访问凭证。运行本代码示例之前，请确保已设置环境变量OSS_ACCESS_KEY_ID和OSS_ACCESS_KEY_SECRET。
+//        EnvironmentVariableCredentialsProvider credentialsProvider = CredentialsProviderFactory.newEnvironmentVariableCredentialsProvider();
+    private static final String ACCESS_KEY_ID = "LTAI5tSBSPcZ5sqftNiVkZts";
+    private static final String ACCESS_KEY_SECRET = "a9Iwh9oS3crMmRoND4L1HMRZrqz0Z3";
+    // 填写Bucket名称，例如examplebucket。
+    private static final String BUCKETNAME = "sky-fire-itcast";
 
 
 
